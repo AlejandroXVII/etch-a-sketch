950d06b17b05208073d4c4038e0b36b01380317f
AlejandroXVII <m.alejandro.xvii@gmail.com>
2023-11-19 23:44:40 -0400

    Create a listener that drawn on the canvas

--- file.js
+++ file.js
@@ -6,8 +6,20 @@ for (let index = 0; index < canvasSize; index++) {
     for (let y = 0; y < canvasSize; y++) {
         const canvasPixel = document.createElement('div');
         canvasPixel.classList.add('canvasPixel');
-        canvasPixel.setAttribute('style', 'display: display: flex;width: 31.25px;height: 31.25px;background-color: whiteg;')
+        canvasPixel.setAttribute('style', 'display: display: flex;width: 31.25px;height: 31.25px;background-color: white;')
+        canvasPixel.setAttribute('class', 'pix');
         container.appendChild(canvasPixel);
-        console.log(canvasPixel);
+
     }
+}
+
+
+const track = document.querySelectorAll('.pix');
+function colorear() {
+    //this.setAttribute('style', 'display: background-color: black;border:1px solid blue;');
+    this.style.backgroundColor = "red";
+    console.log(this);
+ }
+for (let index = 0; index < canvasSize*canvasSize; index++) {
+    track[index].addEventListener('mousemove',colorear);
 }
