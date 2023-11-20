e5d5f1961e61f0802afdf35d1d60ac66a248d28f
AlejandroXVII <m.alejandro.xvii@gmail.com>
2023-11-20 02:33:22 -0400

    Enable the option to change the size of the canvas

--- file.js
+++ file.js
@@ -1,25 +1,37 @@
 canvasSize=16;
-boxSize=500/16;
-const container = document.querySelector('#paintingZone');
-        
-for (let index = 0; index < canvasSize; index++) {
-    for (let y = 0; y < canvasSize; y++) {
-        const canvasPixel = document.createElement('div');
-        canvasPixel.classList.add('canvasPixel');
-        canvasPixel.setAttribute('style', 'display: display: flex;width: 31.25px;height: 31.25px;background-color: white;')
-        canvasPixel.setAttribute('class', 'pix');
-        container.appendChild(canvasPixel);
-
+function createCanvas(){  
+    boxSize=500/canvasSize;
+    const container = document.querySelector('#paintingZone');
+    for (let index = 0; index < canvasSize; index++) {
+        for (let y = 0; y < canvasSize; y++) {
+            const canvasPixel = document.createElement('div');
+            canvasPixel.classList.add('canvasPixel');
+            canvasPixel.setAttribute('style', 'display: display: flex;width: '+String(boxSize)+'px;height:'+String(boxSize)+'px;background-color: white;')
+            canvasPixel.setAttribute('class', 'pix');
+            container.appendChild(canvasPixel);
+        }
+    }
+    
+    const track = document.querySelectorAll('.pix');
+    function toPain() {
+        //this.setAttribute('style', 'display: background-color: black;border:1px solid blue;');
+        this.style.backgroundColor = "blue";
+        console.log(this);
+    }
+    for (let index = 0; index < canvasSize*canvasSize; index++) {
+        track[index].addEventListener('mousemove',toPain);
     }
 }
+createCanvas();
 
+const range = document.querySelector('.slider');
+range.addEventListener('input', function() {
+    canvasSize=this.value;
+    const pixes = document.querySelectorAll('.pix');
+    //pixes[5].parentElement.removeChild(pixes[5]);
 
-const track = document.querySelectorAll('.pix');
-function colorear() {
-    //this.setAttribute('style', 'display: background-color: black;border:1px solid blue;');
-    this.style.backgroundColor = "red";
-    console.log(this);
- }
-for (let index = 0; index < canvasSize*canvasSize; index++) {
-    track[index].addEventListener('mousemove',colorear);
-}
+    for (let index = 0; index < pixes.length; index++) {
+        pixes[index].parentElement.removeChild(pixes[index]);
+     }
+     createCanvas()
+  }, false);
